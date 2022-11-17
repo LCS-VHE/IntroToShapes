7a7848ba016428a3169ddd745e5d42e6e7c41de8
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-11-17 12:01:55 -0500

    Added a diamond shape

--- IntroToShapes/ContentView.swift
+++ IntroToShapes/ContentView.swift
@@ -25,9 +25,25 @@ struct Diagonal: Shape
     
 }
 
+struct Diamond : Shape
+{
+    func path(in rect: CGRect) -> Path
+    {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.maxX / 2 , y: 0 ))
+        path.addLine(to: CGPoint(x: rect.maxX , y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0 , y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 2 , y: 0))
+
+        
+        return path
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Diagonal()
+        Diamond()
             .stroke()
     }
 }
