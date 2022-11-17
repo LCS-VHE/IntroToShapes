db152ee36f4883dd79eff8f7533cf10de72f2656
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-11-17 11:42:16 -0500

    Initial Commit

---
+++ IntroToShapes/IntroToShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToShapesApp.swift
+//  IntroToShapes
+//
+//  Created by Vincent He on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct IntroToShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
